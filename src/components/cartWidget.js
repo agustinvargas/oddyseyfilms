a8d64c3c90c5d666a5bf3062ff7347132faf49f6
Agustín Vargas Hoffmann <agustinvargas93@gmail.com>
2021-08-02 16:43:44 -0300

    desafío 2

--- src/components/cartWidget.js
+++ src/components/cartWidget.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CartWidget = () => {
     return (
-        <FontAwesomeIcon id="btnCart" icon="shopping-cart" size="lg" className="mx-2 " role="button" />
+        <FontAwesomeIcon id="btnCart" icon="shopping-cart" size="lg" className="mx-2" role="button" />
     )
 }
 
